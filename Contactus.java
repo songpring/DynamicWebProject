966c24fc39b7b8e98c5f91b74111ebfe33418973
Minkyung <IrinKwon@DESKTOP-5O83TPG>
2020-04-27 18:00:08 +0900

    jquery 추가 및 나라입력칸 삽입

---
+++ Contactus.java
@@ -0,0 +1,124 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contactus_model extends CI_Model {
+
+	public $tablename = 'ci_contactus';
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+
+	public function do_listData($data) {
+
+		// insert, update , delete
+		if(!empty($data['oper'])) {
+
+			$r_data = array();
+			$r_data['result'] = 'success';
+
+			if($data['oper']=='edit') {
+				$array = array(
+			        'cate' => $data['cate1']
+				);
+
+				$this->db->where('id', $data['no']);
+				$this->db->update($this->tablename, $array);				
+			}
+			else if($data['oper']=='del') {
+				$no_array = explode(',', $data['no']);
+
+			    foreach($no_array as $key) {  // delete 1 
+
+					$this->db->where('id', $key);
+					$this->db->delete($this->tablename);
+			    }
+
+			}
+
+		    $r_data['oper'] = $data['oper'];
+		    echo json_encode($r_data);
+		    exit;			
+		
+		}
+
+		if(!empty($data['selectedCate'])) $this->db->where('cate', $data['selectedCate']);
+
+		if(!empty($data['search'])) {
+			$this->db->group_start();
+			$array = array('name' => $data['search'], 'nationality' => $data['search'], 'phonenumber' => $data['search'], 'sns' => $data['search'],'instagram' => $data['search'], 'uploadfiles' => $data['search'], 'extends_var' => $data['search']);
+			$this->db->or_like($array);
+			$this->db->group_end();			
+		}
+
+		$this->db->from($this->tablename);
+
+		$user_data = $this->session->userdata('logged_in');
+
+		$responce = new stdClass();
+		$responce->total = $this->db->count_all_results();	// flush_cache
+
+		$this->db->from($this->tablename);
+
+		if(!empty($data['search'])) {
+			$this->db->group_start();
+			$array = array('name' => $data['search'],'nationality' => $data['search'], 'phonenumber' => $data['search'], 'sns' => $data['search'],'instagram' => $data['search'], 'uploadfiles' => $data['search'], 'extends_var' => $data['search']);
+			$this->db->or_like($array);	
+			$this->db->group_end();	
+		}	
+
+		if(!empty($data['selectedCate'])) $this->db->where('cate', $data['selectedCate']);
+
+		$this->db->select(' id, cate, name, nationality, phonenumber, birthday, sns, instagram, uploadfiles, extends_var, reg_datetime ');
+
+
+		//$this->db->where('level', 1);
+
+		$this->db->order_by($data['sort'], $data['order']);
+		$this->db->limit($data['limit'], $data['offset']);
+
+		$query = $this->db->get();
+
+		$responce->rows = array();
+		//$responce->query = $this->db->last_query(); 
+
+		$num = $responce->total - $data['offset'];//$offset + 1;
+		foreach ($query->result() as $row) {
+
+			$row->num = $num;
+			$responce->rows[] = $row;
+
+			$num--;
+		}
+
+		$this->db->flush_cache();
+		$this->db->select(' cate ');
+		$this->db->from($this->tablename);
+		$this->db->where('cate !=', '');
+		$this->db->group_by('cate');
+		$query = $this->db->get();
+		foreach ($query->result() as $row) {
+			$responce->cates[] = $row;
+		}
+
+
+		return json_encode($responce);
+	}	
+
+	public function do_register($data) {
+
+		$this->db->set($data);
+		$this->db->insert($this->tablename);
+
+		$lastid = $this->db->insert_id();
+
+		return $lastid;
+	}
+
+	public function get_category() {
+
+		$this->db->from($this->tablename);
+		$query = $this->db->get();
+	}
+}
---
+++ ContactusController.java
@@ -0,0 +1,170 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contactus extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+
+
+	}
+
+	public function index()	{
+		$this->load->helper('form');		
+		$this->load->view('contactus/form');
+	}
+
+
+	public function register() {
+		if(!$this->input->post('name', TRUE)) header('Location: /contactus');
+
+		$this->load->model('contactus_model');
+		$arr_key = array('name', 'nationality','phonenumber','birthday','sns', 'instagram','uploadfiles');
+		$extends_var = '';
+		foreach($_POST as $key => $val) {
+			if(!in_array($key, $arr_key)) {
+				$extends_var[$key] = $this->input->post($key);
+			}
+		}
+
+		$data = array(
+			'name' => $this->input->post('name', TRUE),
+			'nationality' => $this->input->post('nationality', TRUE),
+			'phonenumber' => $this->input->post('phonenumber', TRUE),
+			'birthday' => date('Y-m-d', strtotime($this->input->post('birthday', TRUE))),
+			'sns' => $this->input->post('sns', TRUE),
+			'instagram' => $this->input->post('instagram', TRUE),
+			'uploadfiles' => $this->input->post('uploadfiles', TRUE),
+			'extends_var' => json_encode($extends_var),
+			'reg_datetime' => date('Y-m-d H:i:s'),	
+			'reg_ip' => $this->input->ip_address()	
+		);
+
+		$result = $this->contactus_model->do_register($data);
+
+		$data['result'] = $result;
+		header('Location: /contactus/complete');
+		//$this->load->view('contactus/complete', $data);
+	}
+
+	public function complete() {
+		$this->load->view('contactus/complete');
+	}
+
+	public function view() {
+
+		if (!$this->session->has_userdata('logged_in')) {
+		  header('Location: /auth');
+		  exit; 
+		}
+
+		$this->load->view('contactus/list');
+	}
+
+	public function list_data() {
+
+		if (!$this->session->has_userdata('logged_in')) {
+		  header('Location: /auth');
+		  exit; 
+		}		
+		
+		$this->load->model('contactus_model');
+
+		$limit = $this->input->get('limit', TRUE);
+		$offset = $this->input->get('offset', TRUE);
+		$order = $this->input->get('order', TRUE);
+		$sort = $this->input->get('sort', TRUE);
+
+		$limit = isset($limit) ? intval($limit) : 10;
+		$offset = isset($offset) ? intval($offset) : 0;
+		$order = isset($order) ? $order : 'asc';
+		$sort = isset($sort) ? $sort : 'id'; // 기본설정 
+
+		$data = array(
+			'limit' => $limit,
+			'offset' => $offset,
+			'order' => $order,
+			'sort' => $sort,
+			'search' => $this->input->get('search', TRUE),
+			'oper' => $this->input->post('oper', TRUE),	
+			'no' => $this->input->post('no', TRUE),	
+			'selectedCate' => $this->input->get('selectedCate', TRUE),	
+			'cate1' => $this->input->post('cate1', TRUE)
+		);
+
+		$result = $this->contactus_model->do_listData($data);
+
+		echo $result;
+	}
+
+	public function set() {
+
+		if (!$this->session->has_userdata('logged_in')) {
+		  header('Location: /auth');
+		  exit; 
+		}
+
+		$user_data = $this->session->userdata('logged_in');
+
+		// 최고 관리자만 접근
+		if( $user_data['level'] < 9 ) {
+			header('Location: /');
+		  	exit;
+		}
+		
+		$this->load->view('contactus/set-form');
+	}
+
+	public function setlist_data() {
+
+		if (!$this->session->has_userdata('logged_in')) {
+		  header('Location: /auth');
+		  exit; 
+		}		
+		
+		$this->load->model('setform_model');
+
+		$limit = $this->input->get('limit', TRUE);
+		$offset = $this->input->get('offset', TRUE);
+		$order = $this->input->get('order', TRUE);
+		$sort = $this->input->get('sort', TRUE);
+
+		$limit = isset($limit) ? intval($limit) : 10;
+		$offset = isset($offset) ? intval($offset) : 0;
+		$order = isset($order) ? $order : 'asc';
+		$sort = isset($sort) ? $sort : 'id'; // 기본설정 
+
+		$data = array(
+			'limit' => $limit,
+			'offset' => $offset,
+			'order' => $order,
+			'sort' => $sort,
+			'search' => $this->input->get('search', TRUE),
+			'oper' => $this->input->post('oper', TRUE),	
+			'no' => $this->input->post('no', TRUE),	
+			'tablename' => 'ci_contactus',
+			'var_use' => $this->input->post('var_use', TRUE),	
+			'var_type' => $this->input->post('var_type', TRUE),	
+			'var_name' => $this->input->post('var_name', TRUE),	
+			'var_values' => $this->input->post('var_values', TRUE),	
+			'var_require' => $this->input->post('var_require', TRUE)			
+		);
+
+		$result = $this->setform_model->do_listData($data);
+
+		echo $result;
+	}
+
+	public function get_form_data() {
+
+		$this->load->model('setform_model');
+		$data = array(
+			'tablename' => 'ci_contactus'
+		);
+		$result = $this->setform_model->get_form_data($data);
+
+		echo $result;		
+	}
+
+
+}
